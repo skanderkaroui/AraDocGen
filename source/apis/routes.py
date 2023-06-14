4bcc7b9d4d7123e58d565545d44226aa5441e3ae
skanderkaroui <skander.karoui@gmail.com>
2023-06-14 16:01:35 +0100

    added multiple images

--- source/apis/routes.py
+++ source/apis/routes.py
@@ -6,7 +6,7 @@ from source.apis.utils import generate_pdf, get_available
 
 router = APIRouter()
 
-text = Path(r"/source/models/data.txt").read_text(encoding="utf-8")
+text = Path(r"C:\Users\skand\PycharmProjects\DocGeneratorFastApi\source\models\data.txt").read_text(encoding="utf-8")
 
 
 @router.get("/")
@@ -20,7 +20,7 @@ async def say_hello(name: str):
 
 
 @router.get("/generate-doc")
-async def generate_document(n_pages=1, font_size=12, fontype="font1"):
+async def generate_document(n_pages=10, font_size=12, fontype="font1"):
     pdf_buffer = generate_pdf(text, n_pages, font_size, fontype)
     return Response(content=pdf_buffer.getvalue(), media_type="application/pdf",
                     headers={"Content-Disposition": "attachment; filename=generated_doc.pdf"})
--- source/apis/utils.py
+++ source/apis/utils.py
@@ -4,48 +4,57 @@ import fitz
 
 font1 = fitz.Font(fontfile=r"C:\Users\skand\PycharmProjects\DocGeneratorFastApi\source\models\AF_Diwani-Normal-Traditional.ttf")
 font2 = fitz.Font(fontfile=r"C:\Users\skand\PycharmProjects\DocGeneratorFastApi\source\models\andlso.ttf")
+img = open(r"C:\Users\skand\PycharmProjects\DocGeneratorFastApi\source\models\image.jpg", "rb").read()
 
 
 
-def generate_pdf(text, n_pages=1, font_size=12, fonttype=font1):
-    # open the document
-    doc = fitz.open()
-    # insert a new page
-    page = doc.new_page()
-    # reshape the text to connect the arabic words together
-    text_reshaped = arabic_reshaper.reshape(text)
-    # initializing the text writer
-    text_writer = fitz.TextWriter(page.rect)
-    # first block of text
-    text_writer.fill_textbox(
-        (300, 100, 550, 800),
-        text_reshaped,
-        font=fonttype,
-        fontsize=int(font_size),
-        align=3,
-        right_to_left=True
-    )
-    # second block of text
-    text_writer.fill_textbox(
-        (30, 100, 280, 350),
-        text_reshaped,
-        font=fonttype,
-        fontsize=int(font_size),
-        align=3,
-        right_to_left=True
-    )
-    # image generation ( code not ready )
-    text_writer.write_text(page)  # write text on each page
+def generate_pdf(text, n_pages=10, font_size=12, fonttype=font1):
+    doc = fitz.open()  # open the document
+    for i in range(int(n_pages)):
+        page = doc.new_page()
+        page = doc[i]
+        # reshape the text to connect the arabic words together
+        text_reshaped = arabic_reshaper.reshape(text)
+        # initializing the text writer
+        text_writer = fitz.TextWriter(page.rect)
+        # first block of text
+        text_writer.fill_textbox(
+            (300, 100, 550, 800),
+            text_reshaped,
+            font=fonttype,
+            fontsize=int(font_size),
+            align=3,
+            right_to_left=True
+        )
+        # second block of text
+        text_writer.fill_textbox(
+            (30, 100, 280, 350),
+            text_reshaped,
+            font=fonttype,
+            fontsize=int(font_size),
+            align=3,
+            right_to_left=True
+        )
+        page.insert_image(
+            (30, 100, 280, 1085),
+            stream=img,
+        )
+
+        text_writer.write_text(page)
+
     out = fitz.open()  # output PDF
+
     # making the pdf non-readable
     for page in doc:
         w, h = page.rect.br  # page width / height taken from bottom right point coords
         outpage = out.new_page(width=w, height=h)  # out page has same dimensions
         pix = page.get_pixmap(dpi=150)  # set desired resolution
         outpage.insert_image(page.rect, pixmap=pix)
+
     out_buffer = BytesIO()
     out.save(out_buffer)
     out_buffer.seek(0)
+
     return out_buffer
 
 
