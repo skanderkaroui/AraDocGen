39b14c2253c6995ea6a72973a19872a8776e71f0
skanderkaroui <skander.karoui@gmail.com>
2023-06-21 09:50:17 +0100

    added a function to extract content from the wiki page

--- source/services/utils.py
+++ source/services/utils.py
@@ -1,75 +1,128 @@
-from io import BytesIO
-import arabic_reshaper
 import fitz
-
+import arabic_reshaper
+from io import BytesIO
+import requests
+import bs4
 
 img = open(r"C:\Users\skand\PycharmProjects\DocGeneratorFastApi\source\models\image.jpg", "rb").read()
+class Aradocgen:
+    def generate_pdf(self, fonttype, text, n_pages=10, font_size=12):
+        doc = fitz.open()  # open the document
+        for i in range(int(n_pages)):
+            page = doc.new_page()
+            page = doc[i]
+            # reshape the text to connect the arabic words together
+            text_reshaped = arabic_reshaper.reshape(text)
+            # initializing the text writer
+            text_writer = fitz.TextWriter(page.rect)
+            # first block of text
+            text_writer.fill_textbox(
+                (300, 100, 550, 800),
+                text_reshaped,
+                font=fonttype,
+                fontsize=int(font_size),
+                align=3,
+                right_to_left=True
+            )
+            # second block of text
+            text_writer.fill_textbox(
+                (30, 100, 280, 350),
+                text_reshaped,
+                font=fonttype,
+                fontsize=int(font_size),
+                align=3,
+                right_to_left=True
+            )
+            page.insert_image(
+                (30, 100, 280, 1085),
+                stream=img,
+            )
 
+            text_writer.write_text(page)
 
+        out = fitz.open()  # output PDF
 
-def generate_pdf(fonttype, text, n_pages=10, font_size=12):
-    doc = fitz.open()  # open the document
-    for i in range(int(n_pages)):
-        page = doc.new_page()
-        page = doc[i]
-        # reshape the text to connect the arabic words together
-        text_reshaped = arabic_reshaper.reshape(text)
-        # initializing the text writer
-        text_writer = fitz.TextWriter(page.rect)
-        # first block of text
-        text_writer.fill_textbox(
-            (300, 100, 550, 800),
-            text_reshaped,
-            font=fonttype,
-            fontsize=int(font_size),
-            align=3,
-            right_to_left=True
-        )
-        # second block of text
-        text_writer.fill_textbox(
-            (30, 100, 280, 350),
-            text_reshaped,
-            font=fonttype,
-            fontsize=int(font_size),
-            align=3,
-            right_to_left=True
-        )
-        page.insert_image(
-            (30, 100, 280, 1085),
-            stream=img,
-        )
+        # making the pdf non-readable
+        for page in doc:
+            w, h = page.rect.br  # page width / height taken from bottom right point coords
+            outpage = out.new_page(width=w, height=h)  # out page has same dimensions
+            pix = page.get_pixmap(dpi=150)  # set desired resolution
+            outpage.insert_image(page.rect, pixmap=pix)
 
-        text_writer.write_text(page)
+        out_buffer = BytesIO()
+        out.save(out_buffer)
+        out_buffer.seek(0)
 
-    out = fitz.open()  # output PDF
+        return out_buffer
 
-    # making the pdf non-readable
-    for page in doc:
-        w, h = page.rect.br  # page width / height taken from bottom right point coords
-        outpage = out.new_page(width=w, height=h)  # out page has same dimensions
-        pix = page.get_pixmap(dpi=150)  # set desired resolution
-        outpage.insert_image(page.rect, pixmap=pix)
+    def get_available(self):
+        available_fonts = [
+            "Advertising Bold",
+            "Af-Diwani",
+            "Andalus",
+            "Arabic transparent",
+            "Arslan Wessam A",
+            "Decotype Thuluth",
+            "M-Unicode Sara",
+            "Decotype Naskh",
+            "Simplified Arabic",
+            "Tahoma",
+            "Traditional Arabic",
+        ]
+        font_size_range = [10, 20]  # Example font size range
+        return available_fonts, font_size_range
 
-    out_buffer = BytesIO()
-    out.save(out_buffer)
-    out_buffer.seek(0)
+    def extract_content_from_website(url):
+        response = requests.get(url)
+        soup = bs4.BeautifulSoup(response.content, 'html.parser')
 
-    return out_buffer
+        # Extract title
+        title = soup.find('span', class_='mw-page-title-main')
+        if title:
+            title_text = title.get_text()
+        else:
+            title_text = ""
 
+        # Extract paragraphs, images, captions, and headlines
+        content_blocks = []
+        counter = 1
 
-def get_available():
-    available_fonts = [
-        "Advertising Bold",
-        "Af-Diwani",
-        "Andalus",
-        "Arabic transparent",
-        "Arslan Wessam A",
-        "Decotype Thuluth",
-        "M-Unicode Sara",
-        "Decotype Naskh",
-        "Simplified Arabic",
-        "Tahoma",
-        "Traditional Arabic",
-    ]
-    font_size_range = [10, 20]  # Example font size range
-    return available_fonts, font_size_range
+        paragraph_tags = soup.find_all('p')
+        image_tags = soup.find_all('img', class_='thumbimage')
+        image_captions = soup.find_all('div', class_='thumbcaption')
+        mw_headlines = soup.find_all('span', class_='mw-headline')
+
+        for paragraph_tag, image_tag, caption, headline in zip(paragraph_tags, image_tags, image_captions,
+                                                               mw_headlines):
+
+            text = paragraph_tag.get_text().strip()
+            src = image_tag['src']
+            caption_text = caption.get_text()
+            headline_text = headline.get_text()
+
+            if text:
+                content_blocks.append({
+                    'type': 'paragraph',
+                    'content': text,
+                    'number': counter
+                })
+                counter += 1
+
+            if src:
+                content_blocks.append({
+                    'type': 'image',
+                    'src': src,
+                    'number': counter,
+                    'caption': caption_text
+                })
+                counter += 1
+
+            if headline_text:
+                content_blocks.append({
+                    'type': 'headline',
+                    'content': headline_text,
+                    'number': counter
+                })
+                counter += 1
+
+        return title_text, content_blocks, counter
