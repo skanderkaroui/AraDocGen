ada789249eff9b430313de312d2be19e2c9f851b
skanderkaroui <skander.karoui@gmail.com>
2023-07-10 14:32:00 +0100

    camelCase

--- source/models/Arabic_Fonts/fonts.py
+++ source/models/Arabic_Fonts/fonts.py
@@ -1,14 +1,35 @@
+from enum import Enum
+
+
+class FontEnum(Enum):
+    advertisingBold = "advertisingBold"
+    afDiwani = "afDiwani"
+    andalus = "andalus"
+    arabicTransparent = "arabicTransparent"
+    arslanWessamA = "arslanWessamA"
+    decotypeNaskh = "decotypeNaskh"
+    decotypeThuluth = "decotypeThuluth"
+    mUnicodeSara = "mUnicodeSara"
+    sakkalMajalla = "sakkalMajalla"
+    simplifiedArabic = "simplifiedArabic"
+    tahoma = "tahoma"
+    traditionalArabic = "traditionalArabic"
+
+    def get_font_path(self):
+        return fonts.get(self.value)
+
+
 fonts = {
-    "AdvertisingBold": r"models\Arabic_Fonts\AdvertisingBold.ttf",
-    "AfDiwani": r".\source\models\Arabic_Fonts\AfDiwani.ttf",
-    "Andalus": r".\source\models\Arabic_Fonts\Andalus.ttf",
-    "ArabicTransparent": r".\source\models\Arabic_Fonts\ArabicTransparent.ttf",
-    "ArslanWessamA": r".\source\models\Arabic_Fonts\ArslanWessamA.ttf",
-    "DecotypeNaskh": r".\source\models\Arabic_Fonts\DecotypeNaskh.ttf",
-    "DecotypeThuluth": r".\source\models\Arabic_Fonts\DecotypeThuluth.ttf",
-    "MUnicodeSara": r".\source\models\Arabic_Fonts\MUnicodeSara.ttf",
-    "SakkalMajalla": r".\source\models\Arabic_Fonts\SakkalMajalla.ttf",
-    "SimplifiedArabic": r".\source\models\Arabic_Fonts\SimplifiedArabic.ttf",
-    "Tahoma": r".\source\models\Arabic_Fonts\Tahoma.ttf",
-    "TraditionalArabic": r".\source\models\Arabic_Fonts\TraditionalArabic.ttf"
+    "advertisingBold": r"models\Arabic_Fonts\AdvertisingBold.ttf",
+    "afDiwani": r".\source\models\Arabic_Fonts\AfDiwani.ttf",
+    "andalus": r".\source\models\Arabic_Fonts\Andalus.ttf",
+    "arabicTransparent": r".\source\models\Arabic_Fonts\ArabicTransparent.ttf",
+    "arslanWessamA": r".\source\models\Arabic_Fonts\ArslanWessamA.ttf",
+    "decotypeNaskh": r".\source\models\Arabic_Fonts\DecotypeNaskh.ttf",
+    "decotypeThuluth": r".\source\models\Arabic_Fonts\DecotypeThuluth.ttf",
+    "mUnicodeSara": r".\source\models\Arabic_Fonts\MUnicodeSara.ttf",
+    "sakkalMajalla": r".\source\models\Arabic_Fonts\SakkalMajalla.ttf",
+    "simplifiedArabic": r".\source\models\Arabic_Fonts\SimplifiedArabic.ttf",
+    "tahoma": r".\source\models\Arabic_Fonts\Tahoma.ttf",
+    "traditionalArabic": r".\source\models\Arabic_Fonts\TraditionalArabic.ttf"
 }
